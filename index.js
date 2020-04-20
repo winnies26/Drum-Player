44e2903b64e49ce8ad2e7e7efc107a413cdef360
winnies26 <winnie.sun.26@gmail.com>
2020-04-20 12:15:29 -0700

    Add sound when either clicked the drum pictures of press corresponding keyboard

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-for (var i = 0; i < (document.querySelectorAll(".drum").length); i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-      alert("i got click");
-    });
+// Button
+// all class with drum    loop through i   add eventlisten to i
+var numDrum = (document.querySelectorAll(".drum").length);
+for (var i = 0; i < numDrum; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio('sounds/kick.mp3');
+      kick.play();
+      break;
+    case "l":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    default:
+      consol.log("WRONG");
   }
+
+}
